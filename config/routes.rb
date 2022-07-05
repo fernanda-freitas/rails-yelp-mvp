50af8b76c58c61862272f7a051aeadb56cb2a7ce
Fernanda Freitas Martinez <idea.fernanda@gmail.com>
2022-07-05 20:23:34 +0100

    routes definition

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,3 @@ Rails.application.routes.draw do
     resources :reviews, only: [ :new, :create ]
   end
 end
-
-# Create restaurant - new
-# Create restaurant - create
-# Read all - index
-# Read one - show
-  # Create review for restaurant - new
-  # Create review to restaurant - create

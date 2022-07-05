67b4ea9c2a541d76673ca809f3402527548a7450
Fernanda Freitas Martinez <idea.fernanda@gmail.com>
2022-07-05 20:23:20 +0100

    routes definition

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :restaurants, only: [ :new, :create, :index, :show ] do
+    resources :reviews, only: [ :new, :create ]
+  end
 end
+
+# Create restaurant - new
+# Create restaurant - create
+# Read all - index
+# Read one - show
+  # Create review for restaurant - new
+  # Create review to restaurant - create

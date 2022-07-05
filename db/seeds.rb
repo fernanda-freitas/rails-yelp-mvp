db388c7929544b92659cc25554667c3311949f61
Fernanda Freitas Martinez <idea.fernanda@gmail.com>
2022-07-05 20:12:42 +0100

    migration and seeding done

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+dishoom = { name: "Dishoom", address: "7 Boundary St, London E2 7JE", category: ["chinese", "italian", "japanese", "french", "belgian"].sample }
+pizza_east =  { name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", category: ["chinese", "italian", "japanese", "french", "belgian"].sample }
+rebel_asian =  { name: "Rebel Asian", address: "Rua da Esperança, 164, Lisboa", category: ["chinese", "italian", "japanese", "french", "belgian"].sample }
+cow_burger =  { name: "Cow Burger", address: "Avenida Borges Coelho, 52, Lisboa", category: ["chinese", "italian", "japanese", "french", "belgian"].sample }
+aguas_livres =  { name: "Águas Livres", address: "Rua Otaviano Costa, 123, Lisboa", category: ["chinese", "italian", "japanese", "french", "belgian"].sample }
+
+[ dishoom, pizza_east, rebel_asian, cow_burger, aguas_livres ].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
